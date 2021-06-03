a5f7332dfae125d0f6a6f865da772382209492ed
Lamzira Mohammed <mohamed.lamzira-prestataire@laposte.fr>
2021-06-04 01:07:38 +0200

    refactor tests

--- src/test/java/domain/AccountTest.java
+++ src/test/java/domain/AccountTest.java
@@ -2,13 +2,20 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
 
+  private Account account;
+
+  @BeforeEach
+  void setUp() {
+    account = new Account();
+  }
+
   @Test
   void account_balance_should_be_empty_at_first() {
-    Account account = new Account();
     Balance zeroBalance = new Balance(0L);
 
     assertThat(account.balance()).isEqualTo(zeroBalance);
@@ -16,8 +23,6 @@ class AccountTest {
 
   @Test
   void account_balance_should_be_10EUR_after_making_a_deposit_of_10EUR() {
-    Account account = new Account();
-
     account.deposit(new Amount(1000L));
 
     assertThat(account.balance()).isEqualTo(new Balance(1000L));
@@ -25,8 +30,6 @@ class AccountTest {
 
   @Test
   void account_balance_should_be_20EUR_after_making_two_deposits_of_10EUR() {
-    Account account = new Account();
-
     account.deposit(new Amount(1000L));
     account.deposit(new Amount(1000L));
 
@@ -35,7 +38,6 @@ class AccountTest {
 
   @Test
   void account_balance_should_be_5EUR_after_making_a_withdrawal_of_5EUR() {
-    Account account = new Account();
     account.deposit(new Amount(1000L)); // initial deposit
 
     account.withdraw(new Amount(500L));
@@ -45,7 +47,6 @@ class AccountTest {
 
   @Test
   void account_balance_should_be_empty_after_making_two_withdrawals_of_5EUR() {
-    Account account = new Account();
     account.deposit(new Amount(1000L)); // initial deposit
 
     account.withdraw(new Amount(500L));
